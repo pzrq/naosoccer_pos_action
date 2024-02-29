4415105521a8cb87ebc98f6c29c884f29dd1bc3c
Antonio Bono <antonio.bono@unical.it>
2024-02-29 11:45:36 +0100

    launch file added

---
+++ nao_pos_server/launch/swing_launch.py
@@ -0,0 +1,18 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    return LaunchDescription([
+        Node(
+            package='nao_pos_server',
+            #namespace='turtlesim2',
+            executable='nao_pos_action_server',
+            name='nao_pos_action_server_node'
+        ),
+        Node(
+            package='nao_pos_server',
+            #namespace='turtlesim2',
+            executable='nao_pos_action_client',
+            name='nao_pos_action_client_node'
+        ),
+    ])
