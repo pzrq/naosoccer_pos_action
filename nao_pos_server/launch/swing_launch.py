3a1c200b6e03a547ea83e90c9a930e7cc76e753c
Antonio Bono <antonio.bono@unical.it>
2024-04-20 13:51:36 +0200

    new launch file

--- nao_pos_server/launch/swing_launch.py
+++ nao_pos_server/launch/swing_launch.py
@@ -7,18 +7,36 @@ def generate_launch_description():
             package='nao_pos_server',
             #namespace='turtlesim2',
             executable='nao_pos_action_server',
-            name='nao_pos_action_server_node'
+            name='nao_pos_action_server_legs',
+            remappings=[
+                ('nao_pos_action/_action/feedback', 'nao_pos_action_legs/_action/feedback'),
+                ('nao_pos_action/_action/status', 'nao_pos_action_legs/_action/status'),
+                ('nao_pos_action/_action/cancel_goal', 'nao_pos_action_legs/_action/cancel_goal'),
+                ('nao_pos_action/_action/get_result', 'nao_pos_action_legs/_action/get_result'),
+                ('nao_pos_action/_action/send_goal', 'nao_pos_action_legs/_action/send_goal'),
+            ],
         ),
         Node(
             package='nao_pos_server',
             #namespace='turtlesim2',
             executable='nao_pos_action_client',
-            name='nao_pos_action_client_node'
+            name='nao_pos_action_client_legs',
+            remappings=[
+                ('nao_pos_action/_action/feedback', 'nao_pos_action_legs/_action/feedback'),
+                ('nao_pos_action/_action/status', 'nao_pos_action_legs/_action/status'),
+                ('nao_pos_action/_action/cancel_goal', 'nao_pos_action_legs/_action/cancel_goal'),
+                ('nao_pos_action/_action/get_result', 'nao_pos_action_legs/_action/get_result'),
+                ('nao_pos_action/_action/send_goal', 'nao_pos_action_legs/_action/send_goal'),
+                ('action_req', 'action_req_legs'),
+            ],
         ),
         Node(
             package='nao_pos_server',
             #namespace='turtlesim2',
             executable='nao_pos_publisher',
-            name='nao_pos_publsher_node'
+            name='nao_pos_publisher_legs',
+            remappings=[
+                ('action_req', 'action_req_legs'),
+            ],
         ),
     ])
