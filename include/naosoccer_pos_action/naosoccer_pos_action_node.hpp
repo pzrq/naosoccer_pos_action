85517bd6f9b083e7ae21b941fae922e4f70a37aa
Peter Schmidt <peter@peterjs.com>
2025-01-07 10:43:27 +1100

    chore: First get the legal stuff out of the way
    https://opensource.stackexchange.com/questions/9774/how-to-fill-the-name-of-copyright-owner-in-the-license-if-there-are-multiple#9777

--- include/naosoccer_pos_action/naosoccer_pos_action_node.hpp
+++ include/naosoccer_pos_action/naosoccer_pos_action_node.hpp
@@ -1,4 +1,5 @@
 // Copyright 2023 Kenji Brameld
+// Copyright 2025 Peter Schmidt
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
